818138fec32423dae4f0662e5042ef1cf7b0fa08
developftu <developftu@developftu.net.frontuari>
2023-11-07 09:42:58 -0400

    se documento el funcionamiento de lectura de ruta con el metodo del protocolo http

--- lib/characters/http/character.dart
+++ lib/characters/http/character.dart
@@ -5,6 +5,7 @@ import 'dart:convert' as convert;
 
 Future <Map<String, dynamic>> getCharacters() async {
 
+//Espera dos argumentos el primero es el dominio host, y el segundo es la ruta del recurso en ese dominio
 
 var url = Uri.https('rickandmortyapi.com', '/api/character');
 
